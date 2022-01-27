0061dbd22cfd78a392234ac5fc5ede8fe3a63842
EunSung <82389915+EnSung@users.noreply.github.com>
2022-01-27 12:09:43 +0900

    Update class_Grammer.cpp

--- cpp/class_Grammer.cpp
+++ cpp/class_Grammer.cpp
@@ -10,6 +10,9 @@ using namespace std;
 
 // 원래는 따로 cpp 파일을 만들어 함수 구현부를 따로 빼주는것이 편함
 
+#pragma region 클래스
+
+#pragma region Stock
 class Stock
 {
 	// private : 외부에서 접근 불가, 외부에서 접근하기위해선 public으로 선언된 함수를 통해 값을 변경해야한다.
@@ -37,13 +40,13 @@ private:
 
 // :: - 사용 범위결정 연산자  
 
-void Stock::buy(int n , float pr) {// Stock클래스의 사용범위에 결정된다로 받아드려짐.
+void Stock::buy(int n, float pr) {// Stock클래스의 사용범위에 결정된다로 받아드려짐.
 	shares += n;
-	share_val = pr ;
+	share_val = pr;
 	set_total();
 
 }
-void Stock::sell(int n , float pr) {
+void Stock::sell(int n, float pr) {
 	shares -= n;
 	share_val = pr;
 	set_total();
@@ -71,8 +74,8 @@ Stock::Stock(string co, int n, float pr)
 
 Stock& Stock::topval(Stock& s) {
 	if (s.share_val > share_val) {
-			return s;
-		} 
+		return s;
+	}
 	else {
 		return *this; // 자기 자신  this는 포인터기때문에 주소를 나타낸다. 그래서 *를 붙여 객체를 리턴해줌 
 	}
@@ -90,6 +93,93 @@ Stock::~Stock()
 {
 	cout << name << "클래스가 소멸되었습니다.\n";
 }
+#pragma endregion
+
+#pragma region Time
+
+
+class Time
+{ 
+private:
+	int hours;
+	int mins;
+public:
+	void addHours(int);
+	void addMins(int);
+	//Time sum(Time&);
+	Time operator+(Time&); // sum함수 이름을 operator+
+	void show();
+	Time();
+	Time(int,int);
+	~Time();
+	Time operator*(int);
+	friend Time operator*(int, Time&); // 이 원형은 두가지 함축적인 의미를 가지고있다.
+	// operator*는 클래스 안에 선언되었지만 멤버함수가 아니다. 그러므로 우리가 평소에 호출하기위해 썼던 "." 이나 "->"를 통해 호출할 수 없다.
+	// 멤버함수는 아니지만 멤버함수와 동등한 접근 권한이 있다.
+	//
+
+private:
+
+};
+
+Time::Time()
+{
+	hours = mins = 0;
+}
+
+Time::Time(int h, int m)
+{
+	hours = h;
+	mins = m;
+}
+Time::~Time()
+{
+}
+
+void Time::addHours(int h) {
+	hours += h;
+}
+void Time::addMins(int m) {
+	mins += m;
+	hours += mins / 60;
+	mins %= 60;
+}
+	
+Time Time::operator+(Time& t) {
+	Time sum;
+	sum.mins = mins + t.mins;
+	sum.hours = hours + t.hours;
+	sum.hours += sum.mins / 60;
+	sum.mins %= 60;
+	return sum;
+}
+void Time::show() {
+	cout << "시간 : " << hours << endl;
+	cout << "분 : " << mins << endl;
+
+}
+
+Time operator*(int n, Time& t) { // 구현할 떈 멤버 함수가 아니므로 Time:: 와 friend 키워드를 빼준다.
+	/*Time result;
+	long resultMin = t.hours * n * 60 + t.mins * n;
+	result.hours = resultMin / 60;
+	result.mins = resultMin % 60;*/
+	return t * n;	  
+}
+
+Time Time::operator*(int n) {
+	Time result;
+	long resultMin = hours * n * 60 + mins * n;
+	result.hours = resultMin / 60;
+	result.mins = resultMin % 60;
+	return result;
+}
+
+#pragma endregion
+
+#pragma endregion
+
+
 
 
 
@@ -178,6 +268,65 @@ Stock s1("A", 10, 1000);
 #pragma endregion
 
 #pragma region 연산자 오버로딩
+	 //연산자 오버로딩은 함수오버로딩 개념을 연산자로 가져온것
+	 // 한 가지 연산자로 여러 역할을 하는 것
+	 // ex) * : 주소에 적용될 시 주소에 있는 값을 산출하거나 값의 곱을 산출할 수 있음.
+	 // 
+	 // 연산자 오버로딩을 이용해 우리가만든 형식(클래스,...)을 연산을 할 수 있다.
+	 // 선언 : 연산을 하는함수 이름을 "operator" + "하고자하는 연산기호"
+	 // ex) void operator+
+	 // 
+	 //
+
+	Time day1(1, 40);
+	Time day2(2, 30);
+	
+	day1.show();
+	cout << endl;
+	day2.show();
+	cout << endl;
+
+	Time total;
+	//total = day1.sum(day2); 
+	total = day1.operator+(day2); // 사용법1
+	total = day1 + day2; // 사용법2
+	total.show();
+
+	cout << endl;
+
+#pragma endregion
+
+#pragma region 프랜드
+	
+	//friend : public 이외에도 private로 접근 할 수 있는 통로.
+	// 어떤 클래스의 이항 연산자를 오버로딩하면 friend를 쓸 필요가 생김.
+	// 이항 연산자 : 두 개의 피연산자를 요구하는 연산자.
+	// ex
+	// Time 클래스의 operator*를 만드려고한다.
+	//
+
+	Time a, b;
+	//a = b * 3; // 이것은 operator+처럼 Time과 Time이 연산하는 것이아닌 Time과 int의 연산 통해 Time클래스 값에 저장하는 방식
+	//a = b.operator*(3);  // 위의 것을 풀어쓰면 이러한 식이 된다. 매개변수로 Time클래스가아닌 int형 변수이기 때문에 
+						   //연산자 오버로딩으론 해결할 수 없는 문제가 생긴다. 그래서 프렌드를 쓰게된다.
+	
+	Time t1(1, 20);
+	Time t2;
+
+	t2 = 3 * t1;
+
+	t2.show();
+
+	// 이러면 문제점이생긴다. 3 * t1이아닌 t1 * 3으로 쓰게될 경우 매개변수의 위치가 달라 작동하지않는다.
+	// freind키워드를 쓴 operator*를 오버로딩하는 방법도 있지만
+	// freind키워드를 쓰지않고 operator(int)를 선언해주는 방법도있다.
+	// 그후 freind키워드를 쓴 operator*의 내용을 return t * n으로 바꿔준다면 매개변수의 순서가 어찌되던 연산이된다.
+	
+	// 이렇게되면 friend operator*은 Time의 private 멤버변수에 접근하지 않기 때문에 friend키워드가 없어도 되지만
+	// 써주는것이 바람직한 코딩 방법이다.
+	// 클래스의 공식적인 함수중 하나이기 떄문이다.
+
+
 
 #pragma endregion
 
